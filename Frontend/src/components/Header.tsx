b1ed6433e4162b89e3893285a5cf296e9901570d
ayan878 <ayanraza197@gmail.com>
2024-04-18 23:27:47 +0530

    header

--- Frontend/src/components/Header.tsx
+++ Frontend/src/components/Header.tsx
@@ -1,9 +1,8 @@
 import { Link } from "react-router-dom";
-import logo from "../assets/logo.svg"
-import { ShoppingCartIcon } from "lucide-react";
-import Search from "./Search";
-import { Button } from "./ui/button";
+import logo from "../assets/logo.svg";
 import MobileNav from "./MobileNav";
+import MainNav from "./MainNav";
+
 export default function Header() {
   return (
     <div className="border-b-2 shadow-sm py-6 ">
@@ -16,46 +15,11 @@ export default function Header() {
             <img src={logo} />
           </Link>
         </div>
-
-        <div className=" container mx-auto flex justify-between gap-4 items-center">
-          <Link
-            to="/"
-            className="text- font-bold tracking-tight text-black text-base"
-          >
-            Home
-          </Link>
-          <Link
-            to="/"
-            className="text-base font-bold tracking-tight text-black"
-          >
-            About
-          </Link>
-          <Link
-            to="/"
-            className="text-base font-bold tracking-tight text-black"
-          >
-            Course
-          </Link>
-          <Link
-            to="/"
-            className="text-base font-bold tracking-tight text-black"
-          >
-            Blog
-          </Link>
-          <Link
-            to="/"
-            className="text-base font-bold tracking-tight text-black"
-          >
-            Contact
-          </Link>
-        </div>
-        <div className=" container mx-auto flex justify-between gap-4 items-center">
-          <Search />
-          <ShoppingCartIcon className="text-black text-2xl" />
-          <Button className="bg-green-700 px-8">Login</Button>
-        </div>
         <div className="md:hidden">
           <MobileNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
