3a85c86e1fc39c312010ab06edcbc8918b14d09e
ayan878 <ayanraza197@gmail.com>
2024-04-19 22:54:34 +0530

    layout

--- Frontend/src/layouts/Layout.tsx
+++ Frontend/src/layouts/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
 type Props = {
@@ -8,6 +9,7 @@ function Layout({children}: Props) {
   return <div className="flex flex-col min-h-screen">
     <Header/>
     <div className="container mx-auto flex-1 py-10">{children}</div>
+    <Footer/>
   </div>;
 }
 
