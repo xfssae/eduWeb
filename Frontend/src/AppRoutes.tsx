cef6cccaa358df2a386f0beb300be5e287f0d87a
ayan878 <ayanraza197@gmail.com>
2024-04-19 22:52:20 +0530

    App route

--- Frontend/src/AppRoutes.tsx
+++ Frontend/src/AppRoutes.tsx
@@ -1,10 +1,11 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import HomePage from "./components/pages/HomePage";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Layout>Home Page</Layout>} />
+      <Route path="/" element={<Layout><HomePage/></Layout>} />
       <Route path="/user-profile" element={<span>User-Profile-Page</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
