c57ce3893a8ef922dedaff24a793bbcd6bf231ed
ayan878 <ayanraza197@gmail.com>
2024-04-19 22:52:53 +0530

    main nav

--- Frontend/src/components/MainNav.tsx
+++ Frontend/src/components/MainNav.tsx
@@ -1,33 +1,34 @@
-import { Search, ShoppingCartIcon } from "lucide-react";
+import { Search, ShoppingCart } from "lucide-react";
 import { Button } from "./ui/button";
 import { Link } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 export default function MainNav() {
   return (
     <div className="container flex">
-      <div className=" container mx-auto flex justify-between gap-4 items-center">
+      <div className=" container mx-auto flex justify-between gap-4 items-center hover:text-green-700">
         <Link
           to="/"
-          className="text- font-bold tracking-tight text-black text-base"
+          className="text-base font-bold tracking-tight text-black hover:text-green-700"
         >
           Home
         </Link>
-        <Link to="/" className="text-base font-bold tracking-tight text-black">
+        <Link to="/" className="text-base font-bold tracking-tight text-black hover:text-green-700">
           About
         </Link>
-        <Link to="/" className="text-base font-bold tracking-tight text-black">
+        <Link to="/" className="text-base font-bold tracking-tight text-black hover:text-green-700">
           Course
         </Link>
-        <Link to="/" className="text-base font-bold tracking-tight text-black">
+        <Link to="/" className="text-base font-bold tracking-tight text-black hover:text-green-700">
           Blog
         </Link>
-        <Link to="/" className="text-base font-bold tracking-tight text-black">
+        <Link to="/" className="text-base font-bold tracking-tight text-black hover:text-green-700">
           Contact
         </Link>
       </div>
       <div className=" container mx-auto flex justify-between gap-4 items-center">
-        <Search />
-        <ShoppingCartIcon className="text-black text-2xl" />
+        <SearchBar/>
+        <ShoppingCart className="text-black cursor-pointer text-2xl hover:text-green-700" />
         <Button className="bg-green-700 px-8">Login</Button>
       </div>
     </div>
