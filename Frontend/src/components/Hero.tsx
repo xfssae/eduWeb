4675b8a8a56b208ede61396d91bf206a06e966d1
ayan878 <ayanraza197@gmail.com>
2024-04-21 05:52:07 +0530

    hero

--- Frontend/src/components/Hero.tsx
+++ Frontend/src/components/Hero.tsx
@@ -5,13 +5,13 @@ import { Button } from "./ui/button";
 
 export default function Hero() {
   return (
-    <div className="bg-heroBg bg-cover w-screen h-screen flex flex-col flex-1 p-24 md:flex-row gap-8">
-      <div className="w-full md:w-2/3 flex flex-col text-center  ">
-        <h2 className=" md:text-left mb-2 text-2xl md:text-6xl tracking-tight leading-tight font-bold md:mt-6">
+    <div className="bg-heroBg bg-cover min-h-screen w-screen overflow-hidden flex flex-col flex-1 p-8 md:p-24 md:flex-row gap-8">
+      <div className=" md:w-2/3 flex flex-col text-center  ">
+        <h2 className=" md:text-left mb-8 text-2xl md:text-6xl tracking-tight leading-tight font-bold md:mt-6">
           The Best Program to <span className="text-red-500">Enroll</span> For
           Exchange
         </h2>
-        <p className="text-xl md:text-left mb-4">
+        <p className="text-xl md:text-left mb-8 md:w-[400px]">
           India's largest learning platfom for Computer Science Student
         </p>
         <Button className="bg-green-700 w-28 p-8 mx-auto md:mx-0 ">
@@ -29,7 +29,7 @@ export default function Hero() {
           alt="right"
           className=" w-64 h-80 rounded-br-[90px] rounded-tl-[90px] md:mt-20"
         />
-        <div className=" absolute top-[500px] right-68 bg-white rounded-br-[48px] rounded-tl-[48px] w-72 h-18 p-4 hidden md:flex items-center gap-4">
+        <div className=" absolute top-[440px] right-68 bg-white rounded-br-[48px] rounded-tl-[48px] w-72 h-18 p-4 hidden md:flex items-center gap-4 shadow-lg">
           <Phone className="text-red-500 rounded-full w-14 h-14 py-3 bg-gray-300" />
 
           <div className="flex flex-col ">
