488b776e523a14ad2fcf6c378ab9ce858e26b0bf
ayan878 <ayanraza197@gmail.com>
2024-04-21 14:09:08 +0530

    implement Auth provider

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
+import Auth0ProviderNavigate from "./auth/Auth0ProviderNavigate";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
-      <AppRoutes />
+      <Auth0ProviderNavigate>
+        <AppRoutes />
+      </Auth0ProviderNavigate>
     </Router>
   </React.StrictMode>
 );
