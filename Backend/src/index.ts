0c15cf3cbe4d2adbba6cb30a1d22f6219c5858da
ayan878 <ayanraza197@gmail.com>
2024-04-22 15:13:40 +0530

    use route

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,8 @@
+import express, { Request, Response } from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import express, { Request, Response } from "express";
 import "dotenv/config";
+import myUserRoute from "./routes/myUserRoute"
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connect to database"));
@@ -13,6 +14,8 @@ app.use(cors());
 app.get("/test", async (req: Request, res: Response) => {
   res.json({ message: "Hello!" });
 });
+
+app.use("api/my/user",myUserRoute)
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
 });
