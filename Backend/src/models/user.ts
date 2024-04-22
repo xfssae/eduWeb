5f5f8b0924b1904e2c3220b6585e1a45de988ab1
ayan878 <ayanraza197@gmail.com>
2024-04-22 14:38:48 +0530

    created user schema

---
+++ Backend/src/models/user.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userScehma = new mongoose.Schema({
+  authId: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+
+const User = mongoose.model("User", userScehma);
+export default User;
