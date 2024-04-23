d94deb3de03b11fd7b09124a5f7ccdb6e1664657
ayan878 <ayanraza197@gmail.com>
2024-04-23 16:58:38 +0530

    footer

--- Frontend/src/components/Footer.tsx
+++ Frontend/src/components/Footer.tsx
@@ -6,34 +6,29 @@ import logoLight  from "../assets/logo-light.svg";
 function Footer() {
   return (
     <footer className="bg-footerBg bg-black">
-      <div className="footer-top section">
-        <div className="container grid-list">
+      <div className="">
+        <div className="container md:flex gap-4 p-4">
           <div className="footer-brand">
             <Link href="#" className="logo">
-              <img
-                src={logoLight}
-                width="162"
-                height="50"
-                alt="EduWeb logo"
-              />
+              <img src={logoLight} width="162" height="50" alt="EduWeb logo" />
             </Link>
 
-            <p className="footer-brand-text">
+            <p className="text-gray-400">
               The ultimate resource for computer science students! Whether
               you're just starting out or looking to enhance your skills our
               comprehensive tutorials and guides cover everything.
             </p>
 
-            <div className="wrapper">
-              <span className="span">Add:</span>
+            <div className=" my-4">
+              <span className="text-gray-400">Add:</span>
 
-              <address className="address">
+              <address className="text-gray-400 ml-8">
                 Plot No.2, Sector 17-A, YamunLinkExpressway, Gautam Buddh Nagar,
                 Greater Noida, Uttar Pradesh 201308
               </address>
             </div>
 
-            <div className="wrapper">
+            <div className="text-gray-400 ml-8 ">
               <span className="span">Call:</span>
 
               <Link href="tel:+011234567890" className="footer-link">
@@ -41,7 +36,7 @@ function Footer() {
               </Link>
             </div>
 
-            <div className="wrapper">
+            <div className="text-gray-400 ml-8">
               <span className="span">Email:</span>
 
               <Link href="mailto:info@eduweb.com" className="footer-link">
@@ -50,9 +45,11 @@ function Footer() {
             </div>
           </div>
 
-          <ul className="footer-list">
+          <ul className="text-gray-400">
             <li>
-              <p className="footer-list-title">Online Platform</p>
+              <p className="text-gray-300 text-xl font-bold py-4">
+                Online Platform
+              </p>
             </li>
 
             <li>
@@ -92,9 +89,9 @@ function Footer() {
             </li>
           </ul>
 
-          <ul className="footer-list">
+          <ul className="footer-list text-gray-400 py-4">
             <li>
-              <p className="footer-list-title">Links</p>
+              <p className="text-xl font-bold text-gray-300">Links</p>
             </li>
 
             <li>
@@ -134,10 +131,10 @@ function Footer() {
             </li>
           </ul>
 
-          <div className="footer-list">
-            <p className="footer-list-title">Contacts</p>
+          <div className="">
+            <p className="text-xl font-bold text-gray-300 ">Contacts</p>
 
-            <p className="footer-list-text">
+            <p className="text-gray-400 mb-2">
               Enter your email address to register to our newsletter
               subscription
             </p>
@@ -148,16 +145,16 @@ function Footer() {
                 name="email_address"
                 placeholder="Your email"
                 required
-                className="input-field"
+                className="w-full rounded-sm p-2 mb-2"
               />
 
-              <Button type="submit" className="btn has-before">
-                <span className="span">Subscribe</span>
+              <Button type="submit" className="bg-green-700 w-full my-2">
+                <span>Subscribe</span>
                 <ArrowRight />
               </Button>
             </form>
 
-            <ul className="social-list">
+            <ul className="social-list flex gap-2 text-gray-400">
               <li>
                 <Link href="#" class="social-link">
                   <Facebook />
@@ -166,25 +163,25 @@ function Footer() {
 
               <li>
                 <Link href="#" class="social-link">
-                 <Linkedin/>
+                  <Linkedin />
                 </Link>
               </li>
 
               <li>
                 <Link href="#" class="social-link">
-                 <Instagram/>
+                  <Instagram />
                 </Link>
               </li>
 
               <li>
                 <Link href="#" class="social-link">
-               <Twitter/>
+                  <Twitter />
                 </Link>
               </li>
 
               <li>
                 <Link href="#" class="social-link">
-                 <Youtube/>
+                  <Youtube />
                 </Link>
               </li>
             </ul>
@@ -192,17 +189,10 @@ function Footer() {
         </div>
       </div>
 
-      <div className="border-t-2 shadow-lg">
-        <div className="container">
-          <p className="copyright">
-            Copyright 2023 All Rights Reserved by
-            <Link href="#" className="copyright-link">
-              eduweb
-            </Link>
-            Copyright 2022 All Rights Reserved by
-            <Link href="#" className="copyright-link">
-              codewithBlal&Ayan
-            </Link>
+      <div className="border-t shadow-2xl p-4">
+        <div className="container mx-auto flex justify-center">
+          <p className="text-gray-400">
+            Copyright 2023 All Rights Reserved by EduWeb.
           </p>
         </div>
       </div>
