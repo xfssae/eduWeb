8a10dc3d355c2e7c58bf3cc65e93702a34220cd5
ayan878 <ayanraza197@gmail.com>
2024-04-23 15:37:50 +0530

    myUserController

--- Backend/src/controllers/MyUserController.ts
+++ Backend/src/controllers/MyUserController.ts
@@ -1,11 +1,29 @@
-import { Request,Response } from "express";
+import { Request, Response } from "express";
+import User from "../models/user";
 
-const createCurrentUser=async(req:Request,res:Response)=>{
-    // 1. check if the User exists
-    // 2. create the  user if it doesn't exist
-    // 3. retrun user object  the frontend (calling client)
-}
+const createCurrentUser = async (req: Request, res: Response) => {
+  try {
+    // 1. Check if the user exists
+    const { auth0Id } = req.body;
+    const existingUser = await User.findOne({ auth0Id });
+
+    if (existingUser) {
+      // 2. Return existing user if found
+      return res.status(200).json(existingUser.toObject());
+    }
+
+    // 3. Create a new user if it doesn't exist
+    const newUser = new User(req.body);
+    await newUser.save();
+
+    // Return the newly created user object to the client
+    res.status(201).json(newUser.toObject());
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
+};
 
 export default {
-    createCurrentUser,
-}
+  createCurrentUser,
+};
